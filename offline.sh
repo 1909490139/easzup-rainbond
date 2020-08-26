dec49312f6979f443fa18cf962421b10114f948b
liushuai <linux2573@163.com>
2020-08-26 13:15:37 +0800

    update

--- offline.sh
+++ offline.sh
@@ -26,7 +26,7 @@ function build_kubeasz_image(){
 
     ansible_image_list
 
-    docker build -t registry.cn-hangzhou.aliyuncs.com/goodrain/kubeasz:${KUBEASZ_VER} ./docker
+    docker build -t registry.cn-hangzhou.aliyuncs.com/goodrain/kubeasz:${KUBEASZ_VER} .
     docker login  --username=lius@goodrain --password=${HUBPASS} registry.cn-hangzhou.aliyuncs.com
     docker push registry.cn-hangzhou.aliyuncs.com/goodrain/kubeasz:${KUBEASZ_VER}
 }
